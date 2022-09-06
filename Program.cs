f83d1b7b612936dacee30b9295a06dfd188dc0d4
linaballoons <104442799+linaballoons@users.noreply.github.com>
2022-09-06 18:31:17 +0500

    код программы

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace test
+{
+    static class Program
+    {
+        public static MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root;password= ;database=test");
+
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
