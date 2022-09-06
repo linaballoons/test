60f14969306feb3d959e1ab295605671f3c710cb
linaballoons <104442799+linaballoons@users.noreply.github.com>
2022-09-06 18:29:48 +0500

    код формы приложения

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Program.connection.Open();
+            dataGridView1.Rows.Clear();
+            string query = "SELECT `id`, `amount`, `name` FROM `category` JOIN `tovar` WHERE `id`=`id_tov`";
+            MySqlCommand command = new MySqlCommand(query, Program.connection);
+            MySqlDataReader reader = command.ExecuteReader();
+            List<string[]> data = new List<string[]>();
+            while (reader.Read())
+            {
+                data.Add(new string[3]);
+                data[data.Count - 1][0] = reader[0].ToString();
+                data[data.Count - 1][1] = reader[1].ToString();
+                data[data.Count - 1][2] = reader[2].ToString();
+            }
+            reader.Close();
+            foreach (string[] s in data)
+                dataGridView1.Rows.Add(s);
+            Program.connection.Close();
+        }
+    }
+}
